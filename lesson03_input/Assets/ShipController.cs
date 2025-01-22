1b42f927f415e9927f39286031658aba16db0203
cnobert <cnobert@nait.ca>
2025-01-22 14:54:08 -0700

    end of class

--- lesson03_input/Assets/ShipController.cs
+++ lesson03_input/Assets/ShipController.cs
@@ -3,17 +3,27 @@ using UnityEngine.InputSystem;
 
 public class ShipController : MonoBehaviour
 {
-    public float speed;
-    private InputAction _moveAction;
+    public float movementSpeed, rotationSpeed, scaleSpeed;
+    private InputAction _moveAction, _spinAction, _scaleAction;
     void Start()
     {
         _moveAction = InputSystem.actions.FindAction("Move");
+        _spinAction = InputSystem.actions.FindAction("Spin");
+        _scaleAction = InputSystem.actions.FindAction("Scale");
     }
 
     void Update()
     {
         Vector2 translationValue = _moveAction.ReadValue<Vector2>();
-        translationValue *= Time.deltaTime * speed;
+        translationValue *= Time.deltaTime * movementSpeed;
         transform.Translate(translationValue);
+
+        float spinDirection = _spinAction.ReadValue<float>() * Time.deltaTime;
+        //Debug.Log(spinDirection);
+        transform.Rotate(0, 0, spinDirection * rotationSpeed);
+
+        float scaleAmount = _scaleAction.ReadValue<float>() * Time.deltaTime * scaleSpeed;
+        Vector3 scaleChange = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+        transform.localScale += scaleChange;
     }
 }
