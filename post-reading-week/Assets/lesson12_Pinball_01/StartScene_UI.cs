fc9dd1b3053e7de6ba46074ebec09ca2c95cda6c
cnobert <cnobert@nait.ca>
2025-03-19 13:50:55 -0600

    d

--- post-reading-week/Assets/lesson12_Pinball_01/StartScene_UI.cs
+++ post-reading-week/Assets/lesson12_Pinball_01/StartScene_UI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using System.Security.Cryptography;
 public class StartScene_UI : MonoBehaviour
 {
     private GameStateFinal _gameState;
@@ -14,4 +15,30 @@ public class StartScene_UI : MonoBehaviour
         _highScoreLabel = _root.Q<Label>("HighScoreLabel");
         _currentScoreLabel = _root.Q<Label>("CurrentScoreLabel");
     }
+    void OnEnable()
+    {
+        PopulateScore();
+        _root.Q<Button>("LoadButton").clicked += Load;
+        _root.Q<Button>("SaveButton").clicked += Save;
+        _root.Q<Button>("LoadLevel01Button").clicked += LoadLevel01;
+    }
+    private void PopulateScore()
+    {
+        _highScoreLabel.text = _gameState.HighScore + "";
+        _currentScoreLabel.text = _gameState.CurrentScore + "";
+    }
+    internal void Load()
+    {
+        //_gameState.LoadFromDisk();
+        PopulateScore();
+    }
+    internal void Save()
+    {
+        //_gameState.SaveToDisk();
+    }
+    internal void LoadLevel01()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
 }
