b56b16024339fffdeb70f583ba9922315a3b79e0
Nash Coward <144169845+Bolt27@users.noreply.github.com>
2025-04-15 11:12:52 -0600

    d
    d

--- post-reading-week/Assets/lesson13_platformer/PlatformerController.cs
+++ post-reading-week/Assets/lesson13_platformer/PlatformerController.cs
@@ -3,43 +3,31 @@ using UnityEngine.InputSystem;
 
 public class PlatformerController : MonoBehaviour
 {
-    public float maxSpeed;  //5
-    public float jumpForce; //100
-    public float moveForce; //365
+    public float maxSpeed, jumpForce, moveForce;
     public Transform groundCheck;
     private Rigidbody2D _myRigidBody;
     private Animator _myAnimator;
-    private InputAction _jump;
-    private InputAction _move;
-    private float _oldPosition;
-    private bool _grounded;
-    private bool _initiateJump;
-    private bool _isMovingLeft;
-
-    private void Awake()
+    private InputAction _jump, _move;
+    private bool _grounded, _jumpInitiated;
+    private bool _facingRight;
+    void Awake()
     {
         _myRigidBody = GetComponent<Rigidbody2D>();
         _myAnimator = GetComponent<Animator>();
         _jump = InputSystem.actions.FindAction("Jump");
         _move = InputSystem.actions.FindAction("Move");
-        _initiateJump = false;
-        _isMovingLeft = false;
-        _oldPosition = transform.position.x;
+        _jumpInitiated = false;
+        _facingRight = true;
     }
-    // todo: Update() vs FixedUpdate();
+
+    //todo: Update() vs. FixedUpdate()
     void Update()
     {
-        _oldPosition = transform.position.x;
-        // remember to make all platforms belong to the ground layer
+        //remember to make all platforms belong to the "Ground" layer
         _grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
         if(_jump.WasPressedThisFrame() && _grounded)
         {
-            _initiateJump = true;
-        }
-        if (transform.position.x < _oldPosition)
-        {
-            _isMovingLeft = true;
-            transform.localScale = new Vector3(-1.2f, 1.2f, 1.2f);
+            _jumpInitiated = true;
         }
     }
 
@@ -47,23 +35,35 @@ public class PlatformerController : MonoBehaviour
     {
         float horizontalMovement = _move.ReadValue<Vector2>().x;
 
-        // have we reached _maxSpeed? If not, add force.
-        // It works going left because horizontalMovement and _myRigidBody.Velocity.x
-        // will both be negative at the same time.
-        if (horizontalMovement * _myRigidBody.linearVelocityX < maxSpeed)
+        //Have we reached _maxSpeed? If not, add force.
+        //It works going left because horizonatlMovement and _myRigidBody.velocity.x 
+        //will both be negative at the same time.
+        if(horizontalMovement * _myRigidBody.linearVelocityX < maxSpeed)
         {
             _myRigidBody.AddForce(Vector2.right * horizontalMovement * moveForce);
         }
-        // Have we exceeded maxSpeed? Set to maxSpeed
+        //have we exceeded maxSpeed? If yes, set to maxSpeed.
         if(Mathf.Abs(_myRigidBody.linearVelocityX) > maxSpeed)
         {
-            _myRigidBody.linearVelocity = new Vector2(Mathf.Sign(_myRigidBody.linearVelocityX) * maxSpeed, _myRigidBody.linearVelocityY);
+            _myRigidBody.linearVelocityX = Mathf.Sign(_myRigidBody.linearVelocityX) * maxSpeed;
         }
-        if(_initiateJump)
+        if(_jumpInitiated)
         {
             _myRigidBody.AddForce(new Vector2(0f, jumpForce));
             _myAnimator.SetTrigger("Jump");
-            _initiateJump = false;
+            _jumpInitiated = false;
         }
+        
+        if((horizontalMovement > 0 && !_facingRight) || (horizontalMovement < 0 && _facingRight) )
+        {
+            Flip();
+        }
+    }
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
